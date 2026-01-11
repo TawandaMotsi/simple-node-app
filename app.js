4b490d224da91378e7ce1d5763b1394a25d64375
Tawanda Motsi <a00279643@student.tus.ie>
2026-01-11 18:45:20 +0000

    deployment tag added

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>🚀 Auto Deploy Working!</h1>
+    <p>Node.js auto-deploy test app - deployment</p>
+    <p>Time: ${new Date().toLocaleString()}</p>
+  `);
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
